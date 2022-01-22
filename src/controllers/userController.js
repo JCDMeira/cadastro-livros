74c42b7d0c6bebe33c62bdd062bc9679e411d624
Jean Meira <jean.meira10@hotmail.com>
2022-01-22 16:39:46 -0300

    feat: add user with create user and list all

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,28 @@
 import UserModel from '../models/UserModel';
 
-UserModel.create({
-  name: 'testName',
-})
-  .then((data) => console.log(data))
-  .catch((e) => console.log(e));
+async function createUser({ body }, response) {
+  try {
+    const user = body;
+    console.log(user);
+    const result = await UserModel.create({
+      ...user,
+      created_at: new Date().getTime(),
+    });
+
+    return response.status(201).json(result);
+  } catch ({ message }) {
+    return response.status(400).json({ message });
+  }
+}
+
+async function listUsers(request, response) {
+  try {
+    const result = await UserModel.find({}, { username: 1 });
+
+    return response.status(200).json(result);
+  } catch ({ message }) {
+    return response.status(400).json({ message });
+  }
+}
+
+export default { createUser, listUsers };
--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -1,7 +1,15 @@
+import bcrypt from 'bcryptjs';
 import mongoose from 'mongoose';
 
 const UserSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  created_at: { type: Number, required: true },
+});
+
+UserSchema.pre('save', async function encryptPassword() {
+  const passwordHash = await bcrypt.hash(this.password, 10);
+  this.password = passwordHash;
 });
 
 export default mongoose.model('user', UserSchema);
