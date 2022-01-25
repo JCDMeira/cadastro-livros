3698e6501115cbe1f85e65dc85112dc14c2007d8
Jean Meira <jean.meira10@hotmail.com>
2022-01-24 21:59:36 -0300

    feat: validation token for auth routes

--- src/routes/Auth/books.js
+++ src/routes/Auth/books.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import bookController from '../../controllers/bookController';
+import Auth from '../../middlewares/auth';
 
 const booksRoutes = express.Router();
 
-booksRoutes.post('/book/create', bookController.createBook);
-booksRoutes.get('/books/list', bookController.listBooks);
-booksRoutes.get('/book/find', bookController.findBook);
-booksRoutes.put('/book/edit', bookController.editBook);
-booksRoutes.delete('/book/delete', bookController.deleteBook);
+booksRoutes.post('/book/create', Auth, bookController.createBook);
+booksRoutes.get('/books/list', Auth, bookController.listBooks);
+booksRoutes.get('/book/find', Auth, bookController.findBook);
+booksRoutes.put('/book/edit', Auth, bookController.editBook);
+booksRoutes.delete('/book/delete', Auth, bookController.deleteBook);
 
 export default booksRoutes;
--- src/routes/Auth/user.js
+++ src/routes/Auth/user.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import userController from '../../controllers/userController';
+import Auth from '../../middlewares/auth';
 
 const userRoutes = express.Router();
 
-userRoutes.put('/user/edit', userController.editUser);
-userRoutes.delete('/user/delete', userController.deleteUser);
+userRoutes.put('/user/edit', Auth, userController.editUser);
+userRoutes.delete('/user/delete', Auth, userController.deleteUser);
 
 export default userRoutes;
